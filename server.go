d34c9fad8be550ca7964ea75839edf15ad35df0b
Baluthegoat <141105500+Baluthegoat@users.noreply.github.com>
2024-04-01 22:50:26 +0600

    wip

--- server.go
+++ server.go
@@ -6,53 +6,59 @@ import (
     "log"
     "net/http"
 )
-
-func formHandler(w http.ResponseWriter, r *http.Request) {
-    if err := r.ParseForm(); err != nil {
-        fmt.Fprintf(w, "ParseForm() err: %v", err)
-        return
-    }
-    fmt.Fprintf(w, "POST request successful")
-    name := r.FormValue("name")
-    address := r.FormValue("address")
-    fmt.Fprintf(w, "Name = %s\n", name)
-    fmt.Fprintf(w, "Address = %s\n", address)
-}
-
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-    if r.URL.Path != "/hello" {
-        http.Error(w, "404 not found.", http.StatusNotFound)
-        return
-    }
-
-    if r.Method != "GET" {
-        http.Error(w, "Method is not supported.", http.StatusNotFound)
-        return
-    }
-
-
-    fmt.Fprintf(w, "Hello!")
-}
-
-
-func main() {
-    fileServer := http.FileServer(http.Dir("./static"))
+func main(){
+    fileServer := http.fileServer(http.Dir("./static"))
     http.Handle("/", fileServer)
     http.HandleFunc("/form", formHandler)
     http.HandleFunc("/hello", helloHandler)
+}
+
+// func formHandler(w http.ResponseWriter, r *http.Request) {
+//     if err := r.ParseForm(); err != nil {
+//         fmt.Fprintf(w, "ParseForm() err: %v", err)
+//         return
+//     }
+//     fmt.Fprintf(w, "POST request successful")
+//     name := r.FormValue("name")
+//     address := r.FormValue("address")
+//     fmt.Fprintf(w, "Name = %s\n", name)
+//     fmt.Fprintf(w, "Address = %s\n", address)
+// }
+
+// func helloHandler(w http.ResponseWriter, r *http.Request) {
+//     if r.URL.Path != "/hello" {
+//         http.Error(w, "404 not found.", http.StatusNotFound)
+//         return
+//     }
+
+//     if r.Method != "GET" {
+//         http.Error(w, "Method is not supported.", http.StatusNotFound)
+//         return
+//     }
 
 
-    fmt.Printf("Starting server at port 8080\n")
-    if err := http.ListenAndServe(":8080", nil); err != nil {
-        log.Fatal(err)
-    }
-}
-//handler for the home route//
-func homePageHandler(w http.ResponseWriter, r *http.Request){
-	http.ServeFile(w, r, "./static/index.html")
-}
-// handler for the about route//
-func aboutPageHandler(w http.ResponseWriter, r *http.Request){
-	http.ServeFile(w, r, "./static/about.html")
-}
+//     fmt.Fprintf(w, "Hello!")
+// }
+
+
+// func main() {
+//     fileServer := http.FileServer(http.Dir("./static"))
+//     http.Handle("/", fileServer)
+//     http.HandleFunc("/form", formHandler)
+//     http.HandleFunc("/hello", helloHandler)
+
+
+//     fmt.Printf("Starting server at port 8080\n")
+//     if err := http.ListenAndServe(":8080", nil); err != nil {
+//         log.Fatal(err)
+//     }
+// }
+// //handler for the home route//
+// func homePageHandler(w http.ResponseWriter, r *http.Request){
+// 	http.ServeFile(w, r, "./static/index.html")
+// }
+// // handler for the about route//
+// func aboutPageHandler(w http.ResponseWriter, r *http.Request){
+// 	http.ServeFile(w, r, "./static/about.html")
+// }
 
